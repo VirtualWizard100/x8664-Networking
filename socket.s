170ee1a7a541ffe6b9d3df1f38a0edbfd5d51525
VirtualWizard100 <139145827+VirtualWizard100@users.noreply.github.com>
2025-10-23 14:50:01 -0400

    Add files via upload

--- socket.s
+++ socket.s
@@ -60,11 +60,12 @@ ETH_P_IPV6 equ 0x86DD
 %macro recvfrom 6
 	mov eax, 0x2d
 	mov edi, %1
-	lea esi, %2
+	mov esi, %2
 	mov edx, %3
 	mov r10d, %4
-	lea r8d, %5
-	lea r9d, %6
+	mov r8d, %5
+	mov r9d, %6
+	syscall
 %endmacro
 
 %macro listen 2
