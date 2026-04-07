c3c29a423598db8a3b04e33efdf3b8bdf7c23a0c
VirtualWizard100 <139145827+VirtualWizard100@users.noreply.github.com>
2026-04-07 18:25:05 -0400

    Add files via upload
    Added the hextoasciimacro,s macro found in the x8664 repo, and processed the MAC Address bytes through it.

--- gethwaddress.s
+++ gethwaddress.s
@@ -1,40 +1,50 @@
 %include "socket.s"
 %include "ioctl.s"
+%include "hextoasciimacro.s"
 
 section .text
 
 global _start
 
 _start:
-	socket AF_INET, SOCK_DGRAM, 0
-	mov edi, eax
+        socket AF_INET, SOCK_DGRAM, 0
+        mov DWORD [fd], eax
 
 Ioctl:
-	ioctl edi, SIOCGIFHWADDR, STRUCT_IFREQ
-	jmp Write
-
-Write:
-	mov eax, 0x1
-	mov edi, 0x1
-	mov rsi, [STRUCT_IFREQ + 16 + 2]
-	mov edx, 6
-	syscall
-	jmp exit
+        ioctl [fd], SIOCGIFHWADDR, STRUCT_IFREQ
+loop:
+	mov r8b, BYTE [STRUCT_IFREQ + 16 + 2 + r9]
+	mov [address_buffer + r9], r8b
+	inc r9
+	cmp r9, 0x6
+	je Htoa
+	jmp loop
+Htoa:
+	htoa address_buffer, buflen, ascii_address_buffer
+        jmp exit
 
 exit:
-	mov eax, 0x3c
-	mov edi, 0
-	syscall
+        mov eax, 0x3c
+        mov edi, 0
+        syscall
 
 section .data
 
 struct_sockaddr_ifr_hwaddr:
-	sa_family dw 0
-	sa_data db (14) dup (0)
+        sa_family dw 0
+        sa_data db (14) dup (0)
 
 STRUCT_IFREQ:
         ifr_name db "wlan0", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
         ifr_hwaddr dq struct_sockaddr_ifr_hwaddr
-	ifr_ifindex dd 0
+        ifr_ifindex dd 0
+fd:
+	dd 0
 
+address_buffer:
+	db (6) dup (0)
+buflen equ $-address_buffer
 
+ascii_address_buffer:
+	times buflen*2 db 0
+newlen equ $-ascii_address_buffer
